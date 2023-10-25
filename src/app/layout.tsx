16a0c2c80077ec3dd2fbf3c97142b5d39d7f6292
ElisandroF07 <f07nobru@gmail.com>
2023-10-25 23:31:41 +0100

    fix: :bug: use client colapse with SessionProvider on RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-'use client'
-
 import '@/styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ReactNode } from 'react'
 import { ProgressLoader } from 'nextjs-progressloader'
 import { SessionProvider } from 'next-auth/react'
+import Body from '@/components/body'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,10 +20,7 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang="pt-PT">
-			<body className={inter.className}>
-				<ProgressLoader showSpinner={false} shadow={false} />
-				<SessionProvider>{children}</SessionProvider>
-			</body>
+			<Body className={inter.className} children={children}/>
 		</html>
 	)
 }
